878571e14f7f8212d631702afa2e016d8981a4ea
dm1trykrylov <231014d@gmail.com>
2022-10-06 21:17:19 +0300

    add solution

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+const int kInf = 2e9;
+int number_count;
+int number;
+int min_number = kInf;
+int min_number_index = 0;
+
+bool UpdateMinNumber(int* min_number, const int* new_number) {
+  bool is_updated = false;
+  if (*min_number > *new_number) {
+    *min_number = *new_number;
+    is_updated = true;
+  }
+  return is_updated;
+}
+
+int main() {
+  std::cin >> number_count;
+
+  for (int i = 0; i < number_count; ++i) {
+    std::cin >> number;
+    if (UpdateMinNumber(&min_number, &number)) {
+      min_number_index = i;
+    }
+  }
+
+  std::cout << min_number_index;
+
+  return 0;
+}
