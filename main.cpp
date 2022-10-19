3adf1ce27a68ebdfc4b04dce5d9f3bcaa6874e0b
dm1trykrylov <231014d@gmail.com>
2022-10-20 01:12:12 +0300

    define member-functions outside MinHeap

--- main.cpp
+++ main.cpp
@@ -4,68 +4,88 @@
 
 template <typename T>
 class MinHeap {
+ public:
+  MinHeap() : data_(kCapacity), size_(0) {}
+
+  std::optional<T> Top();
+
+  std::optional<T> ExtractMin();
+
+  void Insert(T value);
+
+  bool Empty() const;
+
+ private:
   static constexpr size_t kCapacity = 8;
   static constexpr size_t kTopIdx = 1;  // heap is 1-indexed
   std::vector<T> data_;
   size_t size_;
 
- public:
-  MinHeap() : data_(kCapacity), size_(0) {}
+  void SiftUp(size_t index);
 
-  std::optional<T> Top() {
-    if (size_ == 0) {
-      return std::nullopt;
-    }
-    return data_[kTopIdx];
-  }
-
-  std::optional<T> ExtractMin() {
-    if (size_ == 0) {
-      return std::nullopt;
-    }
-    std::swap(data_[kTopIdx], data_[size_]);
-    --size_;
-    SiftDown(kTopIdx);
-    return data_[size_ + 1];
-  }
-
-  void Insert(T value) {
-    if (size_ == data_.size() - 1) {
-      data_.resize(2 * size_);
-    }
-    data_[++size_] = value;
-    SiftUp(size_);
-  }
-
-  bool Empty() { return size_ == 0; }
-
- private:
-  void SiftUp(size_t index) {
-    if (index > kTopIdx) {
-      size_t parent = index / 2;
-      if (data_[index] < data_[parent]) {
-        std::swap(data_[index], data_[parent]);
-        SiftUp(parent);
-      }
-    }
-  }
-
-  void SiftDown(size_t index) {
-    size_t i_min = index;
-    size_t i_left_child = index * 2;
-    size_t i_right_child = index * 2 + 1;
-    if (i_left_child <= size_ && data_[i_left_child] < data_[i_min]) {
-      i_min = i_left_child;
-    }
-    if (i_right_child <= size_ && data_[i_right_child] < data_[i_min]) {
-      i_min = i_right_child;
-    }
-    if (index != i_min) {
-      std::swap(data_[i_min], data_[index]);
-      SiftDown(i_min);
-    }
-  }
+  void SiftDown(size_t index);
 };
+
+template <typename T>
+std::optional<T> MinHeap<T>::Top() {
+  if (size_ == 0) {
+    return std::nullopt;
+  }
+  return data_[kTopIdx];
+}
+
+template <typename T>
+std::optional<T> MinHeap<T>::ExtractMin() {
+  if (size_ == 0) {
+    return std::nullopt;
+  }
+  std::swap(data_[kTopIdx], data_[size_]);
+  --size_;
+  SiftDown(kTopIdx);
+  return data_[size_ + 1];
+}
+
+template <typename T>
+bool MinHeap<T>::Empty() const {
+  return size_ == 0;
+}
+
+template <typename T>
+void MinHeap<T>::Insert(T value) {
+  if (size_ == data_.size() - 1) {
+    data_.resize(2 * size_);
+  }
+  data_[++size_] = value;
+  SiftUp(size_);
+}
+
+template <typename T>
+void MinHeap<T>::SiftUp(size_t index) {
+  if (index > kTopIdx) {
+    size_t parent = index / 2;
+    if (data_[index] < data_[parent]) {
+      std::swap(data_[index], data_[parent]);
+      SiftUp(parent);
+    }
+  }
+}
+
+template <typename T>
+void MinHeap<T>::SiftDown(size_t index) {
+  size_t i_min = index;
+  size_t i_left_child = index * 2;
+  size_t i_right_child = index * 2 + 1;
+  if (i_left_child <= size_ && data_[i_left_child] < data_[i_min]) {
+    i_min = i_left_child;
+  }
+  if (i_right_child <= size_ && data_[i_right_child] < data_[i_min]) {
+    i_min = i_right_child;
+  }
+  if (index != i_min) {
+    std::swap(data_[i_min], data_[index]);
+    SiftDown(i_min);
+  }
+}
 
 template <typename T>
 inline std::vector<T> Merge(const std::vector<std::vector<T>>& arrays) {
@@ -95,30 +115,40 @@ inline std::vector<T> Merge(const std::vector<std::vector<T>>& arrays) {
   return result;
 }
 
-int main() {
+void ReadInput(size_t& blocks_count, std::vector<std::vector<int>>** arrays) {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
 
-  size_t blocks_count, block_size, tmp;
   std::cin >> blocks_count;
 
-  std::vector<std::vector<int>>* arrays =
-      new std::vector<std::vector<int>>(blocks_count);
+  *arrays = new std::vector<std::vector<int>>(blocks_count);
 
+  size_t block_size, tmp;
   for (size_t i = 0; i < blocks_count; ++i) {
     std::cin >> block_size;
     for (size_t j = 0; j < block_size; ++j) {
       std::cin >> tmp;
-      (*arrays)[i].push_back(tmp);
+      (**arrays)[i].push_back(tmp);
     }
   }
+}
+
+template <typename T>
+void PrintArray(const T& k_array) {
+  for (auto a : k_array) {
+    std::cout << a << ' ';
+  }
+}
+
+int main() {
+  size_t blocks_count;
+  std::vector<std::vector<int>>* arrays;
+  ReadInput(blocks_count, &arrays);
 
   auto result = Merge<int>(*arrays);
 
-  for (auto x : result) {
-    std::cout << x << ' ';
-  }
+  PrintArray(result);
 
   delete arrays;
   return 0;
