bdc7830bd405b173daa516437a2353508c9b607d
dm1trykrylov <231014d@gmail.com>
2022-12-12 23:13:36 +0300

    Add task5O

--- contest5/contest5-taskO.cpp
+++ contest5/contest5-taskO.cpp
@@ -1,32 +1,69 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
-template <typename T, typename V>
-void CompressValues(std::vector<T>& values) {
-  std::vector<V> tmp;
-  for (auto seg : values) {
-    tmp.emplace_back(seg.l);
-    tmp.emplace_back(seg.r);
-  }
+struct Query {
+  int64_t value;
+  char type;
+};
 
+void CompressValues(std::vector<Query>& queries, std::vector<int64_t>& data) {
+  std::vector<int64_t> tmp(data);
   std::sort(tmp.begin(), tmp.end());
   auto last = std::unique(tmp.begin(), tmp.end());
   tmp.erase(last, tmp.end());
-
-  for (auto& v : values) {
-    v.l = std::lower_bound(tmp.begin(), tmp.end(), v.l) - tmp.begin() + 1;
-    v.r = std::lower_bound(tmp.begin(), tmp.end(), v.r) - tmp.begin() + 1;
+  for (auto& q : queries) {
+    q.value =
+        std::lower_bound(tmp.begin(), tmp.end(), q.value) - tmp.begin() + 1;
   }
 }
 
-template <typename T, typename V>
-void ReadValues(size_t& n, std::vector<T>& values) {
+void ReadQueries(size_t& n, std::vector<Query>& queries) {
   std::cin >> n;
-  values.resize(n);
+  queries.resize(n);
+  std::vector<int64_t> values;
   for (size_t i = 0; i < n; ++i) {
-    std::cin >> values[i].l >> values[i].r;
+    std::cin >> queries[i].type >> queries[i].value;
+    values.push_back(queries[i].value);
   }
   std::sort(values.begin(), values.end());
-  CompressValues<T, V>(values);
+  CompressValues(queries, values);
+}
+
+void FastIO() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+}
+
+void Insert(std::vector<int64_t>& destination, int64_t value) {
+  ++destination[value];
+}
+
+void Erase(std::vector<int64_t>& destination, int64_t value) {
+  destination[value] = 0;
+}
+
+bool Get(std::vector<int64_t>& destination, int64_t value) {
+  return destination[value] > 0;
+}
+
+int main() {
+  // FastIO();
+  size_t n;
+  std::vector<Query> queries;
+  ReadQueries(n, queries);
+  std::vector<int64_t> data(n + 1);
+  for (auto query : queries) {
+    if (query.type == '+') {
+      Insert(data, query.value);
+    }
+    if (query.type == '-') {
+      Erase(data, query.value);
+    }
+    if (query.type == '?') {
+      std::cout << (Get(data, query.value) ? "YES" : "NO") << '\n';
+    }
+  }
+  return 0;
 }
